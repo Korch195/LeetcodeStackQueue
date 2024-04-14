72664f0252297a8dfa48365b3cc3ed9c37b66c7b
Korch195 <korchevyi.pn@ucu.edu.ua>
2024-04-14 20:32:44 +0300

    implemented Queue by using deque

---
+++ Implement Queue using Stacks/solved_with_deque.py
@@ -0,0 +1,37 @@
+from collections import deque
+class MyQueue(object):
+    def __init__(self):
+        self.stack1=deque()
+        self.stack2=deque()
+
+    def push(self, x):
+        """
+        :type x: int
+        :rtype: None
+        """
+        self.stack1.append(x)
+        
+
+    def pop(self):
+        """
+        :rtype: int
+        """
+        if not self.stack2:
+            while self.stack1:
+                self.stack2.append(self.stack1.pop())
+        return self.stack2.pop()
+
+    def peek(self):
+        """
+        :rtype: int
+        """
+        if not self.stack2:
+            while self.stack1:
+                self.stack2.append(self.stack1.pop())
+        return self.stack2[-1]
+
+    def empty(self):
+        """
+        :rtype: bool
+        """
+        return not self.stack1 if not self.stack2 else not self.stack2 
